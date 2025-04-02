9478c105c6d3584f0445dbd26aa9b078a33b442f
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-02 17:27:03 +0000

    Fixed for Volume of Sphere

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha. Vorontsov
+// Created on: March-April 2025
 // This file contains the JS functions for index.html
+
+/**
+ * Displays an alert with the message "Hello, World!"
+ */
+function calculatehoursworkedAndhourlywage() {
+  // input
+  const TaxAmount= (4/3) 
+  const hoursWorked = parseFloat(document.getElementById("radius").value);
+  const hoursworked = parseFloat(document.getElementById("width").value)
+  const hourlyWage = parseFloat(document.getElementById("height").value);
+  let Incometax = hoursWorked^3
+  let takeHomeSalary = TaxAmount * Math.Pi * hoursWorked
+  
+  document.getElementById('answer1').innerHTML = 
+    `Radius: ${Incometax.toFixed(2)} <br>
+    Volume: ${takeHomeSalary.toFixed(2)}`
+
+}
